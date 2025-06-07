14fb97f984131beab08d959e7e6a8aba22c469d1
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-07 22:56:27 +0530

    fix: update MongoDB connection logic

--- src/app/lib/mongo.ts
+++ src/app/lib/mongo.ts
@@ -1,31 +1,24 @@
-// lib/mongo.ts
 import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI!;
 const options = {};
 
-let client: MongoClient;
+let client;
 let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('❌ MONGODB_URI environment variable not set');
+  throw new Error('Please add your Mongo URI to .env.local');
 }
 
 if (process.env.NODE_ENV === 'development') {
-  // In dev mode, use a global variable to preserve value across hot reloads
   if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  // In production, it's best not to use a global variable
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
-export async function connectToDatabase() {
-  const client = await clientPromise;
-  const db = client.db(); // Default DB from URI
-  return { client, db };
-}
+export default clientPromise;
